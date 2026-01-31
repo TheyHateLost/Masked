b5105fabbfddab339d01b0233daa34904a7f4fa2
Michael <thelostkid224@gmail.com>
2026-01-31 11:04:46 -0500

    Movement Controller

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,52 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    //variables for the game
-    float speed;        //determines move speed
-    float move;         //determines direction of movement
+    public float acceleration = 5f;
+    public float steeringSpeed = 200f;
+    private Rigidbody2D rb;
 
-    float jump;         //determines how high the jump is
-    bool isJumping;     //tracks if object is jumping or not
-
-    Rigidbody2D rb;     //place to store the rigidbody of the object
-
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        speed = 10f;                        //set speed value to ten
-        //jump = 400f;                        //set jump value to 400f
-
-        rb = GetComponent<Rigidbody2D>();   //store the rb of the object
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //move the player
-        move = Input.GetAxis("Horizontal");                     //set move to read any of the Unity Horizontal keybinds
+        // Get Input
+        float moveInput = Input.GetAxis("Vertical");
+        float steerInput = Input.GetAxis("Horizontal");
 
-        rb.linearVelocity = new Vector2(speed * move, rb.linearVelocity.y); //move on the x axis(left or right)
+        // Forward Movement: Apply force or move position relative to car's rotation
+        rb.linearVelocity = transform.up * moveInput * acceleration;
 
-        //single jump limit
-        if (Input.GetButtonDown("Jump") && !isJumping)          //when the Unity Jump keybind is presses and if the object is not already jumping
-        {
-            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));     //jump
-            isJumping = true;                                   //set jumping to true
-        }
-    }
-
-    // Called when a Collision is detected
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground"))  //if the other object is tag as ground
-        {
-            isJumping = false;                     //set jumping to false
+        // Steering: Rotate the car based on horizontal input
+        if (moveInput != 0)
+        { // Only steer if moving
+            float rotationAmount = -steerInput * steeringSpeed * Time.fixedDeltaTime;
+            rb.MoveRotation(rb.rotation + rotationAmount);
         }
     }
 }
