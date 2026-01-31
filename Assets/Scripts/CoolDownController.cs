8a5017718db0c4362b6b794e422574ff33cf5b3c
WesWesBoi <159307728+WesWesBoi@users.noreply.github.com>
2026-01-31 12:46:16 -0500

    im2

--- Assets/Scripts/CoolDownController.cs
+++ Assets/Scripts/CoolDownController.cs
@@ -4,41 +4,18 @@ using UnityEngine.UI;
 
 public class CoolDownController : MonoBehaviour
 {
-
-    public Text timerText;              //Demo UI elements
-    public Slider timerSlider;
-
-    public bool active;                 //to activte or deactivate cooldown
-    public float cooldown;              //How often this cooldown may be used
-    public float timer;                 //Time left on timer, can be used at 0
-
-    void Update()
+    public float coolDownTime = 5f; // Cooldown duration in seconds
+    public float ablityTimer;
+    private float cooldown;
+    private void Update()
     {
-        if (active)
-            timer -= Time.deltaTime;    //Subtract the time since last frame from the timer.
-        if (timer < 0)
-            timer = 0;                  //If timer is less than 0, reset it to 0 as we don't want it to be negative
-        UpdateUI();
-    }
-
-    void UpdateUI()
-    {
-        //For purpose of demo
-        timerText.text = "Cooldown: " + timer;
-        timerSlider.value = (timer / cooldown);
-    }
-
-    public void CompleteAction()
-    {
-        if (timer > 0)                  //If timer greater than 0, don't complete action
+        if (ablityTimer > 0)
             return;
 
-        active = false;                 //Set active to be false so timer does not start moving until after action is complete. Remove this if you want timer to restart right away.
+        ablityTimer = cooldown;
+        Debug.Log("Ability Used! Cooldown started.");
 
-        //Run Action Logic
-
-        timer = cooldown;
-
-        active = true;
     }
 }
+
+
